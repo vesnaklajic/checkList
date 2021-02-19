ba7d773cbef3f1948c11eb6bfd603da6070d17bd
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-19 10:51:39 +0100

    Addede main to server link

--- public/main.js
+++ public/main.js
@@ -1,7 +1,24 @@
 var btId=document.querySelector('#addBtn');
 var input=document.querySelector('input');
 
+//on clik btn Create take a value 
 btId.addEventListener('click',()=>{
 let inputVal=input.value;
-console.log(inputVal);
+//connection with server.js which can connect to mongojs
+let xml= new XMLHttpRequest();
+//open and sent the request to DB 
+//method POST to preserve the data from inputVal
+xml.open('post','/save');
+
+//listening to comunication between 2 computers
+xml.onreadystatechange=function(){
+if(xml.readyState==4 && xml.status==200){
+    console.log(xml.responseText);
+}
+}
+//setting xml to send Json
+xml.setRequestHeader('Content-Type','application/json')
+//send a Json format of InputVal to server.js
+//with an object where a msg is inputval
+xml.send(JSON.stringify({msg:inputVal}));
 })
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+const { urlencoded } = require('express');
 const express= require ('express')
 const mongojs=require ('mongojs')
 //DB connection, mongojs will make 'todos' collection 
 const db=mongojs('TODODB',['todos']) 
 const app=express();
 
+//to take a content from a req of POST
+//need a body-parser or express
+app.use(express.json());
+
 //make one static page and use it all the time
 app.use(express.static(__dirname+"/public"));
+
+//mathod 'save' from the main.js
+app.post('/save',(req,res) => {
+    //getting msg property from req body
+    let msg=req.body.msg;
+    //send a msg to a main.js if OK,if we are connected
+    res.send(msg);
+})
 
 app.listen(3000,()=>{
     console.log("Listening to port 3000");
