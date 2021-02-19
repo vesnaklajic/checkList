66acdc38afa62f30a6ae87b5982d575cf680367d
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-19 19:39:52 +0100

    Read DB, make card for each task, delete a card

--- public/main.js
+++ public/main.js
@@ -1,24 +1,90 @@
-var btId=document.querySelector('#addBtn');
-var input=document.querySelector('input');
+//mainRow is party of index.html whis makes a cards
+let mainRow = document.querySelector('#main-row');
+var btId = document.querySelector('#addBtn');
+var input = document.querySelector('input');
 
 //on clik btn Create take a value 
-btId.addEventListener('click',()=>{
-let inputVal=input.value;
-//connection with server.js which can connect to mongojs
-let xml= new XMLHttpRequest();
-//open and sent the request to DB 
-//method POST to preserve the data from inputVal
-xml.open('post','/save');
+btId.addEventListener('click', () => {
+    let inputVal = input.value;
+    //connection with server.js which can connect to mongojs
+    let xml = new XMLHttpRequest();
+    //open and sent the request to DB 
+    //method POST to preserve the data from inputVal
+    xml.open('post', '/save');
 
-//listening to comunication between 2 computers
-xml.onreadystatechange=function(){
-if(xml.readyState==4 && xml.status==200){
-    console.log(xml.responseText);
-}
-}
-//setting xml to send Json
-xml.setRequestHeader('Content-Type','application/json')
-//send a Json format of InputVal to server.js
-//with an object where a msg is inputval
-xml.send(JSON.stringify({msg:inputVal}));
+    //listening to comunication between 2 computers
+    xml.onreadystatechange = function () {
+        if (xml.readyState == 4 && xml.status == 200) {
+            displayTodos();
+        }
+    }
+    //setting xml to send Json
+    xml.setRequestHeader('Content-Type', 'application/json')
+    //send a Json format of InputVal to server.js
+    //with an object where a msg is inputval
+    xml.send(JSON.stringify({ msg: inputVal }));
+
 })
+
+//function to collect data from DB
+function displayTodos() {
+    //var data is a Promise 
+    let data = new Promise((resolve, reject) => {
+        let xml = new XMLHttpRequest();
+        xml.open('get', '/get-data');
+        xml.onreadystatechange = () => {
+            if (xml.readyState == 4 && xml.status == 200) {
+                //what we got back from the server
+                //if OK it will go to .then 
+                resolve(JSON.parse(xml.responseText))
+            }
+        }
+        xml.send();
+    })
+    //when a data arrive run this function 
+    //to make a card for each todo task
+    data.then((data) => {
+        let text = '';
+        //go through all the data from DB 
+        for (let i = 0; i < data.length; i++) {
+            //will make a card 
+            text += `
+    <div class="col-6">
+    <div class="card">
+        <div class="card header">
+           <button class="btn btn-sm btn-secondary float-left">Task${i + 1}</button>
+           <button class="btn btn-sm btn-succes float-right">${data[i].date}</button>
+        </div>
+        <div class="card body text-center">
+            <h3>${data[i].msg}</h3>
+        </div>
+        <div class="card footer text-center">
+            <button data-id="${data[i]._id}" class="btn btn-sm btn-danger">Delete</button>
+        </div>
+    </div>
+</div>`
+        }
+        //using a mainRow to display a card
+        mainRow.innerHTML = text;
+
+        let alldeleteBtns = document.querySelectorAll('[data-id]');
+
+        for (let i = 0; i < alldeleteBtns.length; i++) {
+            alldeleteBtns[i].addEventListener('click', deleteTodo);
+        }
+    })
+}
+displayTodos();
+
+function deleteTodo() {
+    
+    let xml = new XMLHttpRequest();
+    xml.open('post','/del-todo');
+    xml.onreadystatechange = () => {
+        if (xml.readyState == 4 && xml.status == 200) {
+            displayTodos();
+        }
+    }
+    xml.setRequestHeader('Content-Type','application/json')
+    xml.send(JSON.stringify({id:this.getAttribute('data-id')}));
+}
--- server.js
+++ server.js
@@ -1,25 +1,44 @@
 const { urlencoded } = require('express');
-const express= require ('express')
-const mongojs=require ('mongojs')
+const express = require('express')
+const mongojs = require('mongojs')
 //DB connection, mongojs will make 'todos' collection 
-const db=mongojs('TODODB',['todos']) 
-const app=express();
+const db = mongojs('TODODB', ['todos'])
+const app = express();
 
 //to take a content from a req of POST
 //need a body-parser or express
 app.use(express.json());
 
 //make one static page and use it all the time
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
 //mathod 'save' from the main.js
-app.post('/save',(req,res) => {
+app.post('/save', (req, res) => {
     //getting msg property from req body
-    let msg=req.body.msg;
-    //send a msg to a main.js if OK,if we are connected
-    res.send(msg);
+    let msg = req.body.msg;
+    //save a msg into a DB
+    db.todos.insert({ msg: msg, date: new Date().toDateString() }, (err, data) => {
+        //send a msg to a main.js if OK,if we are connected
+        res.send("OK");
+    })
+})
+//route /get-data to collect the data from DB
+app.get('/get-data', (req, res) => {
+    //open the DB , table todos
+    db.todos.find((err, data) => {
+        res.send(data);
+    });
 })
 
-app.listen(3000,()=>{
+app.post('/del-todo', (req, res) => {
+    let id = req.body.id;
+    db.todos.remove({ "_id": db.ObjectId(id) }, (err, data) => {
+        res.send("Deleted")
+    });
+})
+
+
+
+app.listen(3000, () => {
     console.log("Listening to port 3000");
 })
